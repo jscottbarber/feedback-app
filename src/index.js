d27bbff0c14f1cfaec75767138b41d3c2e16d1ab
Scott Barber <jscottbarber@hotmail.com>
2022-12-02 15:46:12 -0700

    put ListApp off to the side while I work on App

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
-// import App from "./App"
-import ListApp from "./ListApp"
+import App from "./App"
+// import ListApp from "./ListApp"
 
 //createRoot()
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(
-  <React.StrictMode>
-    <ListApp />
-  </React.StrictMode>
-)
-
 // root.render(
 //   <React.StrictMode>
-//     <App />
+//     <ListApp />
 //   </React.StrictMode>
 // )
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
